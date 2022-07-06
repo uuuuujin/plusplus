812af6d8c66b567ff2ebd95310dc6701bef58153
[SW2/서울/1반] 정유진 <yearn8546@gmail.com>
2022-07-06 01:20:18 +0000

    setting: prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+	printWidth: 80,
+	parser: 'typescript',
+	singleQuote: true,
+	useTabs: false, // Indent lines with tabs instead of spaces.
+	tabWidth: 2, // Specify the number of spaces per indentation-level.
+};
