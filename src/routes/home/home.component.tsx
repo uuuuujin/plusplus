030035319e97f99c581080d8de9d92817ec8926f
jpark <tmdeksdl@gmail.com>
2022-07-07 12:44:23 +0900

    feat: header 기본 틀

--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,10 +1,14 @@
 import Container from '../../components/container/container.component';
+import Header from '../../components/header/header.component';
 import { Text } from './home.style';
 
 export default function Home(): JSX.Element {
   return (
     <Container>
-      <Text>메인 화면입니다요~</Text>
+      <div>
+        <Header></Header>
+        <Text>메인 화면입니다요~</Text>
+      </div>
     </Container>
   );
 }
