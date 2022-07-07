252805ce53f507c9bd44b36618427a4a962ded57
LYJ <rbdnjs251@naver.com>
2022-07-07 19:09:13 +0900

    feat: 마이페이지 레이아웃 구현

--- src/routes/search/search.component.tsx
+++ src/routes/search/search.component.tsx
@@ -1,7 +1,6 @@
 import Container from '../../components/container/container.component';
 
 export default function Search(): JSX.Element {
-  const a:String = 'adf';
   return (
     <Container>
       <span>검색화면 입니다요~</span>
