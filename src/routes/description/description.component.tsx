c2811c87eb1d34b551b0894b84c3dd3a956f5f54
Sunny-W-Park <vnvnchlrh@korea.ac.kr>
2022-07-08 17:02:06 +0900

    feat: 숙소 설명 페이지 레이아웃 구현 - 반응형, 기본형 완료 & 슬라이더 함수 구현중

--- src/routes/description/description.component.tsx
+++ src/routes/description/description.component.tsx
@@ -1,12 +1,13 @@
 import Container from '../../components/container/container.component';
 import Header from '../../components/header/header.component';
 
-import { AiOutlineCaretLeft, AiOutlineCaretRight } from 'react-icons/ai';
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
 
 import {
-    DescriptionContainer, Intro, Rooms, 
-    Info, Title, Body,
-    Slider, LeftSlideButton, RightSlideButton, ImageBox, 
+    DescriptionContainer, 
+    Intro, Info, Title, Body,
+    Slider, LeftSlideButton, RightSlideButton, ImageBox, SliderImageBox,
+    Rooms, Gallery, RoomDetail,  
 } from './description.style';
 
 export default function Description(): JSX.Element {
@@ -17,25 +18,43 @@ export default function Description(): JSX.Element {
         <DescriptionContainer>
             <Intro>
                 <Title> 스테이 오름 </Title> 
-                <Body> 한 층에 4개의 객실이 자리하고 있으며 객실에 설비된 릴랙세이션 풀은 반얀트리에서의 휴식을 더욱 특별하게 만들어 드립니다. 릴랙세이션 풀에서 스파를 즐기며 아름다운 서울의 전경을 바라보는 시간은 몸과 마음을 채우는 특별한 휴식을 선사합니다.</Body>
+                <Body> 역사적인 왕궁 유적들 사이 유리와 철조로 지어진 건축물을 품은 서울은 전통적인 느낌이 물씬한 동시에 미래 지향적인 감각이 가득한 역동적인 도시입니다. 바닥에서 천장까지 통유리가 설치된 객실에서 밖을 내다보면, 서울의 역동적인 풍경이 한눈에 펼쳐집니다. 광화문에 위치한 현대적인 감각의 포시즌스 호텔 서울은 포시즌스만의 서비스를 선보이는 특별한 공간입니다. 도심 속 스파에서 완벽한 고요와 평화를 만끽하시고 사계절 운영되는 수영장에서 휴식을 취하신 다음, 유유안의 광동식 요리를 맛보세요. 최고급 칵테일로 유명한 찰스 H.바로 오시면, 흥미진진한 스토리가 있는 특별한 칵테일을 즐기실 수 있습니다.</Body>
             </Intro>
 
             <Slider>
-                <LeftSlideButton><AiOutlineCaretLeft/></LeftSlideButton>
-                <ImageBox src="https://www.planetware.com/wpimages/2019/06/south-korea-seoul-best-hotels-four-seasons-hotel-seoul.jpg">
-                </ImageBox>
-                <RightSlideButton><AiOutlineCaretRight/></RightSlideButton>
+                <LeftSlideButton><AiOutlineLeft/></LeftSlideButton>
+                <SliderImageBox src="https://www.planetware.com/wpimages/2019/06/south-korea-seoul-best-hotels-four-seasons-hotel-seoul.jpg">  
+                </SliderImageBox>
+                <RightSlideButton><AiOutlineRight/></RightSlideButton>
             </Slider>
 
             <Rooms>
-                Rooms
+                <Title> 객실 정보 </Title>
+                <Gallery> 
+                  <ImageBox src="https://www.planetware.com/wpimages/2019/06/south-korea-seoul-best-hotels-four-seasons-hotel-seoul.jpg"> 
+                  </ImageBox>
+                  <RoomDetail>기본형</RoomDetail>
+                  <RoomDetail>$50,000 / 1 night </RoomDetail>
+                </Gallery>
 
+                <Gallery> 
+                  <ImageBox src="https://www.planetware.com/wpimages/2019/06/south-korea-seoul-best-hotels-four-seasons-hotel-seoul.jpg"> 
+                  </ImageBox>
+                  <RoomDetail>기본형</RoomDetail>
+                  <RoomDetail>$50,000 / 1 night </RoomDetail>
+                </Gallery>
 
+                <Gallery> 
+                  <ImageBox src="https://www.planetware.com/wpimages/2019/06/south-korea-seoul-best-hotels-four-seasons-hotel-seoul.jpg"> 
+                  </ImageBox>
+                  <RoomDetail>기본형</RoomDetail>
+                  <RoomDetail>$50,000 / 1 night </RoomDetail>
+                </Gallery>
             </Rooms>
 
             <Info>
-                Location and other info
-                
+                <Title> 기타 정보 </Title>
+
             </Info>
         </DescriptionContainer>
       </div>
