6423cb97464476adbb7a1bb8a359ff9492fd372e
jpark <tmdeksdl@gmail.com>
2022-07-17 16:47:54 +0900

    feature: 숙소 리스트 grid 반응형으로 수정

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,15 +3,11 @@ import Description from './description/description.component';
 import Search from './search/search.component';
 import Listing from './listing/listing.component';
 import MyPage from './mypage/mypage.component';
-<<<<<<< HEAD
 import Admin from './admin-page/admin-page.component';
 import UserList from '../components/user-list/user-list.component';
 import RoomList from '../components/room-list/room-list.component';
 import BookingList from '../components/admin-booking-list/admin-booking-list.component';
-=======
 import LoginPage from './login/login.component';
-import AdminPage from './admin-page/admin-page.component';
->>>>>>> 1af99e27c8b0185b0d5e13b6a998afea12dc07a1
 
 export const ROUTES = {
   HOME: {
@@ -43,8 +39,7 @@ export const ROUTES = {
     path: '/login',
     link: '/login',
     component: LoginPage,
-
-<<<<<<< HEAD
+  },
   ADMIN: {
     path: '/admin',
     link: '/admin',
@@ -67,12 +62,5 @@ export const ROUTES = {
     path: '/admin/booking-list',
     link: '/admin/booking-list',
     component: BookingList,
-=======
-    ADMINPAGE: {
-      path: '/adminpage',
-      link: '/adminpage',
-      component: AdminPage,
-    },
->>>>>>> 1af99e27c8b0185b0d5e13b6a998afea12dc07a1
   },
 };
