fe5a24fb213bfecc05689e7d2adfe400615da72d
jpark <tmdeksdl@gmail.com>
2022-07-18 21:27:34 +0900

    feature: admin 유저 리스트 페이지 제작

--- src/components/user-list/user-list.component.tsx
+++ src/components/user-list/user-list.component.tsx
@@ -1,3 +1,50 @@
-export default function UserList(): JSX.Element {
-  return <div></div>;
+import {
+  HeaderText,
+  User,
+  UserDetail,
+  UsersContainer,
+} from './user-list.style';
+
+const userData = [
+  {
+    gender: 'female',
+    phoneNumber: '01012341234',
+    age: 10,
+  },
+  {
+    gender: 'male',
+    phoneNumber: '01043211234',
+    age: 15,
+  },
+  {
+    gender: 'male',
+    phoneNumber: '01066661234',
+    age: 12,
+  },
+  {
+    gender: 'female',
+    phoneNumber: '01012341254',
+    age: 30,
+  },
+];
+interface UserDetailProp {
+  gender: string;
+  phoneNumber: string;
+  age: number;
+}
+
+export default function UserList(props: UserDetailProp): JSX.Element {
+  const { gender, phoneNumber, age } = props;
+  return (
+    <div>
+      <HeaderText>유저 리스트</HeaderText>
+      <UsersContainer>
+        <User>
+          {userData.map((item, key) => {
+            return <UserDetail key={key} gender={item.gender}></UserDetail>;
+          })}
+        </User>
+      </UsersContainer>
+    </div>
+  );
 }
--- src/components/user-list/user-list.style.tsx
+++ src/components/user-list/user-list.style.tsx
@@ -1,1 +1,34 @@
 import styled from 'styled-components';
+import theme from '../../style/theme';
+
+export const HeaderText = styled.span`
+  display: flex;
+  justify-content: center;
+  margin: 30px 0;
+  font-weight: 700;
+  font-size: 32px;
+`;
+
+export const UsersContainer = styled.div`
+  display: flex;
+  width: 66%;
+  border-radius: 8px;
+  border: 1px solid ${theme.colors.border};
+  background: #eee;
+  justify-content: center;
+  flex-direction: column;
+  margin: 20px auto;
+  padding: 10px;
+`;
+
+export const User = styled.div`
+  border-radius: 8px;
+  border: 1px solid ${theme.colors.border};
+  background: white;
+  &:not(:last-of-type) {
+    margin-bottom: 10px;
+  }
+`;
+export const UserDetail = styled.div`
+  background: red;
+`;
